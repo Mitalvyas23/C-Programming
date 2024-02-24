aa882408f7abb85bc3aae39306af19a35c329518
Mital vyas <159806176+Mitalvyas23@users.noreply.github.com>
2024-02-24 08:56:39 -0800

    C Program
    Program 4 -- Number is prime or not using UDF

---
+++ P4.C
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+int prime(int n);
+
+void main()
+{
+int n,p=0;
+clrscr();
+printf("enter a number : ");
+scanf("%d",&n);
+p = prime(n);
+if(p==0)
+{
+printf("number is prime");
+}
+else
+{
+printf("number is not prime");
+}
+getch();
+}
+
+int prime(int n)
+{
+int count=0,i;
+if(n==1 || n==2)
+{
+count=1;
+}
+for(i=2;i<=n/2;++i)
+{
+	if(n % i == 0)
+	{
+	count=1;
+	break;
+	}
+}
+return count;
+}
